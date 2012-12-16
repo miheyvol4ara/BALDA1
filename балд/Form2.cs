2d5431535cf6599d4f8400e24098ff1d55ce1d94
Lordik8066 <lordik8066@mail.ru>
2012-12-16 17:29:22 +0200

    Функция добавления в список слов введенного слова, а также подсчет очков
    Горицан

--- балд/Form2.cs
+++ балд/Form2.cs
@@ -15,6 +15,22 @@ namespace балд
         {
             InitializeComponent();
         }
+         //Функция добавления в список слов введенного слова, а также подсчет очков.
+        static void listadd(ref bool player, ListBox lst1, ListBox lst2, string rts, ref int q1, ref int q2)
+        {
+            if (player)
+            {
+                lst1.Items.Add(rts);
+                player = false;
+                q1 += rts.Length;
+            }
+            else
+            {
+                lst2.Items.Add(rts);
+                player = true;
+                q2 += rts.Length;
+            }
+        }
 
     }
 }
